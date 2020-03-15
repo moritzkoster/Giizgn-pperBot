c2782b2c8433170c52f131f35efaa8db1f473959
Moritz Koster <moritz.koster@bluewin.ch>
2020-03-15 21:26:49 +0100

    new: Nutella

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ from telegram.ext import Updater, MessageHandler, Filters
 with open('token.json', 'r') as token_file:
     token_dict = json.load(token_file)
 
-
 updater = Updater(token=token_dict["token"])
 dispatcher = updater.dispatcher
 
 
-
 def message_handler(bot, update):
+    messageStr = update.message.text.lower().translate({ord(i): None for i in '.:;,?'})
+    if messageStr == "wie isst me nutella":
+        bot.send_message(chat_id=update.message.chat_id, text="Selbstverständlich ohni Butter, nur Scheusale, Barbare und anderi Ranzlinge essed Nutella mit Butter!!!")
     message = update.message.text.lower().split()
+    print(message)
+    if "pflumewäldli" in message:
+        bot.send_message(chat_id=update.message.chat_id, text="Was für ein schöner Ort!")
 
 
 
